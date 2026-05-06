86171d1a51a1960752d165353d35fbe47636fc14
JoMi2026 <jomi5436@gmail.com>
2026-05-06 16:05:16 +0200

    add files

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import './App.css'
 import { Outlet } from 'react-router-dom'
 import Navbar from './Components/Navbar/Navbar'
 import Footer from './Components/Footer/Footer'
-import Vehicules from './pages/Vehicules'
 
 function App() {
 
