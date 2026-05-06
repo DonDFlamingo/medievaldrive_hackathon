0a08068af79bfa6f53b625a62f0a016ba95bdece
JoMi2026 <jomi5436@gmail.com>
2026-05-06 15:57:39 +0200

    add files

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,6 +1,6 @@
 import './Navbar.css'
 import { Link } from 'react-router-dom'
-import Profile from '../../assets/react.svg'
+import Profile from '../../assets/profil.png'
  
 function Navbar(){
     return(
@@ -8,8 +8,8 @@ function Navbar(){
       <Link to="/" className="nav-title">
         <h1 className="title-res">MedievalDrive</h1>
       </Link>
-      <Link to="/profile" className="profile-nav">
-      <img src={Profile} alt="profile"/>
+      <Link to="/profil" className="profile-nav">
+      <img src={Profile} alt="profile" className='img-nav'/>
       </Link>
 
         </nav>
