9aa72b52b69e82636fd5758e184c6e8b77f388aa
Brice Kutuk <bricekutuk@live.fr>
2026-05-07 11:28:01 +0200

    first finish of the voyage page css

--- backend/API/index.js
+++ backend/API/index.js
@@ -1,3 +1,4 @@
+export { calculateTripCost } from "../../src/Components/trajet.js";
 export {
   destinations,
   getDestinationById,
@@ -5,5 +6,4 @@ export {
   getDestinationsByDistanceIndex,
 } from "./destination.js";
 export { getMapById, getMaps, maps } from "./maps.js";
-export { calculateTripCost } from "./trajet.js";
 export { getVehiculeById, getVehicules, vehicules } from "./vehicules.js";
--- src/pages/DetailsDuVoyage/detailsDuVoyage.tsx
+++ src/pages/DetailsDuVoyage/detailsDuVoyage.tsx
@@ -1,6 +1,7 @@
 import {ArrowRightIcon, CardStackIcon, CheckCircledIcon, LapTimerIcon, RocketIcon, StarIcon} from "@radix-ui/react-icons"
 import "./detailsDuVoyage.css"
 import { useEffect,useState } from 'react'
+import Vehicules from "../Vehicules";
 
 
 type maps = {
@@ -9,17 +10,26 @@ type maps = {
     points_interet ?: string;
 }
 
+type Vehicule = {
+    id: number
+    image : string;
+}
+
 
 
 function DetailsDuVoyage () {
     const [maps, setMaps] = useState<maps[]>([])
+    const [vehicule, setVehicule] = useState<Vehicule[]>([])
 
 useEffect(() => {
-  fetch('http://localhost:3001/api/maps')
-    .then((r) => r.json())
-    .then((data) => setMaps(data))
+  Promise.all([
+    fetch('http://localhost:3001/api/maps').then(r => r.json()),
+    fetch('http://localhost:3001/api/vehicules').then(r => r.json()),
+  ]).then(([mapsData, vehiculesData]) => {
+    setMaps(mapsData)
+    setVehicule(vehiculesData)
+  })
 }, [])
-console.log(maps)
     return (
       <div className='pageDetailsDuVoyage'>
         <div className='hautDePagesVoyage'>         
@@ -38,38 +48,41 @@ console.log(maps)
             <div className='imagePageVoyage'>
                 <img src={`http://localhost:3001${maps[2]?.image}`} alt="imagePageVoyage" className='cartePageVoyage' />
                     <div className='btnPageVoyage'>
-                        <button type="button"><CheckCircledIcon/> CONFIRMER LE VOYAGE</button>
-                        <button type="button">MODIFIER LE VOYAGE</button>
+                        <button type="button" className="btnConfirmerVoyage"><CheckCircledIcon/> CONFIRMER LE VOYAGE</button>
+                        <button type="button" className="btnModifierVoyage">MODIFIER LE VOYAGE</button>
                     </div>
             </div>
             <div className='cardPageVoyage'>
                 <div className='statistiqueDeRoute'>
                     <h2 className='h2Voyage'>Statistiques de Route</h2>
                     <div className="distancEtDureeVoyage">
-                        <ul>
-                            <li><RocketIcon className='iconPageVoyage'/> DISTANCE </li>
+                        <ul className="ulDistanceEtVoyage">
+                            <li className="testEspace"><RocketIcon className='iconPageVoyage'/> DISTANCE </li>
                             <li>142 Lieues</li>
-                            {/* <li><LapTimerIcon className='iconPageVoyage'/> DUREE ESTIMEE 3 Soleils</li> */}
                         </ul>
-                        <ul>
-                            <li><LapTimerIcon className='iconPageVoyage'/> DUREE ESTIMEE</li>
+                        <ul className="ulDistanceEtVoyage">
+                            <li className="testEspace"><LapTimerIcon className='iconPageVoyage'/> DUREE ESTIMEE</li>
                             <li>3 Soleils</li>
                         </ul>
                     </div>
-                    <div>
-                        <CardStackIcon className='iconPageVoyage'/> <br />
-                        PRIX TOTAL 3.550 FLORINS D'OR
+                    <div className="prixStatistiqueVoyage">
+                        
+                        <ul className="ulDistanceEtVoyage">
+                            <li className="testEspace"><CardStackIcon className='iconPageVoyage'/> PRIX TOTAL</li>
+                            <li className="prixTotalVoyage">3.550 FLORINS D'OR</li>
+                        </ul>
                     </div>
-                </div>
+                </div>  
                     <div className='transportSelectionerPageVoyage'>
                     <h2 className='h2Voyage'>TRANSPORT SELECTIONNE</h2>
+                    <img src={`http://localhost:3001${vehicule[2]?.image}`} alt="imageVehiculePageVoyage" className='imageVehiculePageVoyage' />
                         <ul>
-                            <li>Noble Coach</li>
-                            <li>Classe Souveraine</li> <StarIcon className='iconPageVoyage'/>
+                            <li className="noblePageVoyage">Noble Coach</li>
+                            <li>Classe Souveraine</li>
                         </ul>
-                        <ul>
-                            <li>Sellerie en velours royal</li>
-                            <li>Escorte de garde personnelle</li>
+                        <ul className="noblePageVoyage">
+                            <li className="liNobleVoyage">Sellerie en velours royal</li>
+                            <li className="liNobleVoyage">Escorte de garde personnelle</li>
                         </ul>
                     </div>
             </div>
